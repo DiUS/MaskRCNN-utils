b4db9fdb40f62e97bd68ef74ae1ee83f7b1e639a
Dil Kumar Pun <dkpun@dius.com.au>
2018-06-06 16:41:55 +1000

    Change filename from _cropped_ to _seg_

--- image_splitter/split.sh
+++ image_splitter/split.sh
@@ -5,6 +5,6 @@ mkdir -p output/{images,masks}
 for images_or_masks in masks images; do
   for file_full_path in ../labelbox_parser/output/${images_or_masks}/*.png; do
     filename=$(basename $file_full_path)
-    convert -crop 1300x1300 $file_full_path "output/${images_or_masks}/${filename}_cropped_%d.png"
+    convert -crop 1300x1300 $file_full_path "output/${images_or_masks}/${filename}_seg_%d.png"
   done
 done
