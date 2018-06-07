499692cc70bac8e60f4be5840ae0c59579179dda
Dil Kumar Pun <dkpun@dius.com.au>
2018-06-07 16:58:08 +1000

    Remove Mask_RCNN subtree

--- Mask_RCNN/vein.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd samples/vein
- time python vein.py train --dataset=../../datasets/vein --subset=train --weights=imagenet
- time python vein.py detect --dataset=../../datasets/vein --subset=val --weights=last
-cd -
